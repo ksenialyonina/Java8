8025907c00df2870e6a31c8d7e20cb37ad1e1c2c
Ksenia Vi <kseniavi.ly@gmail.com>
2023-03-17 19:19:11 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.surname = "Иванов";
+        post.name = "Иван";
+        post.patronymic = "Иванович";
+        post.passport = "4444 № 444444";
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+        post.phone = "+7-999-999-99-99";
+        post.subscription = true;
+
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String surname;
+    public String name;
+    public String patronymic;
+    public String passport;
+    public FormDate birthday;
+    public String phone;
+    public boolean subscription;
+}
